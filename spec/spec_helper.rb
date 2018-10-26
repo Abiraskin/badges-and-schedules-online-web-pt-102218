7ebd3c171e97eabad7c31ec4a08b01f486d073f7
Abi Raskin <abiraskin@gmail.com>
2018-10-26 10:44:38 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require_relative '../conference_badges'
 
 badge_maker = (name)
-puts "Hello, my name is #{name}"
+puts "Hello, my name is #{name}."
 
 badge_maker = ["Edsger", "Ada," "Charles", "Alan", "Grace", "Linus", "Matz"]
 badge_maker.each_with_index do |speaker, index|
