3bbc3f3bc13d46eede2a82ec4f2db9332b2ed102
Abi Raskin <abiraskin@gmail.com>
2018-10-26 10:21:53 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require_relative '../conference_badges'
 
 badge_maker = ["Edsger", "Ada," "Charles", "Alan", "Grace", "Linus", "Matz"]
 badge_maker.each do |name|
-    return "Hello, my name is #{name}."
+    puts "Hello, my name is #{name}."
   end
 
 RSpec.configure do |config|
