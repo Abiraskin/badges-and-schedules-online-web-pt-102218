d53121b38f25227655a209f046d85018d5aa621c
Abi Raskin <abiraskin@gmail.com>
2018-10-26 10:36:37 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require_relative '../conference_badges'
 
 badge_maker = ["Edsger", "Ada," "Charles", "Alan", "Grace", "Linus", "Matz"]
-badge_maker.each_with_index do |speaker, index| 
+badge_maker.each_with_index do |speaker, index|
   puts "Hello, #{speaker}! You'll be assigned to room #{index}!"
 
   end
