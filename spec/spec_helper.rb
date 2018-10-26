fc3306f2fbf3294c5f4a0e52f8ca4d24f68f4e3c
Abi Raskin <abiraskin@gmail.com>
2018-10-26 10:49:52 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,8 @@
 require_relative '../conference_badges'
 
-name = ["Edsger", "Ada," "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-badge_maker = (name)
-puts "Hello, my name is #{badge_maker}."
-
 badge_maker = ["Edsger", "Ada," "Charles", "Alan", "Grace", "Linus", "Matz"]
-badge_maker.each_with_index do |speaker, index|
-  puts "Hello, #{speaker}! You'll be assigned to room #{index}!"
+badge_maker.each do |speaker|
+  puts "Hello, my name is #{speaker}."
 
   end
 
